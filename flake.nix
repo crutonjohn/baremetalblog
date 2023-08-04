404e47c20495b866275302ec9c3aa08df0585f36
Curtis John <admin@curtisrayjohn.com>
2023-08-03 22:03:56 -0400

    might have gotten it!

--- flake.nix
+++ flake.nix
@@ -8,18 +8,20 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-
+      let 
+        pkgs = nixpkgs.legacyPackages.${system}; 
         packageName = throw "baremetalblog";
-
         lib = nixpkgs.lib;
-      in {
-        packages.x86_64-linux.${packageName} = pkgs.callPackage ./nix/pkgs/baremetalblog { };
+      in
+      {
+        packages = rec {
+          baremetalblog = pkgs.callPackage ./nix/pkgs/baremetalblog/hugo.nix { };
+          default = baremetalblog;
+        };
 
-        nixosModules.baremetalblog = import ./nix/modules/baremetalblog/hugo.nix self;
-
-        # defaultPackage = self.packages.${system}.${packageName};
+        nixosModules = rec {
+          baremetalblog = import ./nix/modules/baremetalblog/hugo.nix self;
+        };
 
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
